985f824ea133db7535417ef05d2d4db94a917402
ROC2806 <168469621+ROC2806@users.noreply.github.com>
2025-05-02 17:58:07 +0200

    Add files via upload

---
+++ mongo2.py
@@ -0,0 +1,32 @@
+#import toml
+from urllib.parse import quote_plus
+from pymongo import MongoClient
+import streamlit as st
+
+# secrets.toml einlesen
+#mongo_secrets = toml.load(".streamlit/secrets.toml")
+mongo_secrets = st.secrets["mongodb"]
+# Username und Passwort URL-kodieren
+MONGO_USERNAME = quote_plus(mongo_secrets["MONGO_USERNAME"])
+MONGO_PASSWORD = quote_plus(mongo_secrets["MONGO_PASSWORD"])
+MONGO_CLUSTER = mongo_secrets["MONGO_CLUSTER"]
+#MONGO_CLUSTER = mongo_secrets["mongodb"]["MONGO_CLUSTER"]
+
+# Verbindungs-URI zusammensetzen
+MONGO_URI = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_URI)
+db = client["Wuzzelapp"]
+collection = db["Wuzzelapp_1"]
+
+def load_data():
+    data = collection.find_one({"_id": "app_state"})
+    if data:
+        return data["data"]
+    return {"tournaments": {}, "current_tournament": None}
+
+def save_data(data):
+    collection.update_one(
+        {"_id": "app_state"},
+        {"$set": {"data": data}},
+        upsert=True
+    )
