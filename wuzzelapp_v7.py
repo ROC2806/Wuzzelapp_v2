ff795a8513746a59d705304b6267b5bb487503b6
ROC2806 <168469621+ROC2806@users.noreply.github.com>
2025-07-11 09:30:22 +0200

    Update wuzzelapp_v7.py
    Spielregeln

--- wuzzelapp_v7.py
+++ wuzzelapp_v7.py
@@ -237,6 +237,32 @@ if page == "Turnierverwaltung":
             st.success(f"Ungefähre Spielzeit: {gesamtzeit_stunden:.2f} Stunden")
             st.info(f"Gesamtanzahl der Spiele: {int(gesamt_spiele)}")
 
+    with st.expander("Turnier Spielregeln"):
+        st.markdown("""
+        **Spielmodus:**  
+        - 2 gegen 2  
+        - Gruppenphase (jeder gegen jeden in der Gruppe) und KO-Phase (Viertelfinale, Halbfinale, Finale)
+        - Es gibt 4 Gruppen, die vor Turnierbeginn ausgelost werden.
+        - Die jeweils besten zwei Teams jeder Gruppe steigen in die KO-Phase auf.
+    
+        **Spielzeit:**  
+        - Gruppenphase: 2 Halbzeiten à 4 Minuten  
+        - KO-Phase: 2 Halbzeiten à 7 Minuten
+    
+        **Punktevergabe in Gruppenphase:**  
+        - Sieg: 3 Punkte  
+        - Unentschieden: 1 Punkt
+    
+        **Anstoß & Ballbesitz:**  
+        - Erster Anstoß und bei Halbzeit: Ball wird eingeworfen  
+        - Nach einem Tor: Gegner erhält den Ball  
+        - Mitte zählt, aber nicht beim Anstoß  
+        - Ball im Aus: Jeweilige Verteidigung erhält den Ball
+    
+        **Unentschieden in KO-Phase:**  
+        - Golden Goal mit Einwurf
+        """)
+
 # --- Teams ---
 elif page == "Teams":
     st.header("Bestehende Teams & Gruppenzuordnung")
